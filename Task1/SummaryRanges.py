3bf6f66cdd9de4525e16fefa68eb1bb5bf342fdf
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-10-26 18:00:16 +0300

    add the solution of the LeetCode. Yandex Task List. 228. Summary Ranges and the drafts for multiple tasks

---
+++ Task1/SummaryRanges.py
@@ -0,0 +1,149 @@
+
+
+# LeetCode. Yandex Task List. 228. Summary Ranges
+
+'''
+
+228. Summary Ranges
+Easy
+
+You are given a sorted unique integer array nums.
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+    "a->b" if a != b
+    "a" if a == b
+
+
+
+Example 1:
+
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+
+Example 2:
+
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+
+Example 3:
+
+Input: nums = []
+Output: []
+
+Example 4:
+
+Input: nums = [-1]
+Output: ["-1"]
+
+Example 5:
+
+Input: nums = [0]
+Output: ["0"]
+
+
+
+Constraints:
+
+    0 <= nums.length <= 20
+    -231 <= nums[i] <= 231 - 1
+    All the values of nums are unique.
+    nums is sorted in ascending order.
+
+Accepted
+235,440
+Submissions
+534,903
+
+
+Related Topics
+Array
+
+Similar Questions
+Missing Ranges
+Easy
+Data Stream as Disjoint Intervals
+Hard
+
+'''
+
+
+import math
+import time
+import collections
+from typing import List
+import numpy as np
+import random as rnd
+import itertools as it
+from collections import defaultdict, Counter
+import re
+from functools import reduce
+from functools import lru_cache
+from bisect import bisect, bisect_left
+
+
+class Solution:
+    def convertToStr(self, a: int, b: int) -> str:
+        if a == b:
+            return str(a)
+        return str(a) + "->" + str(b)
+
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        n = len(nums)
+        res = list()
+        if n == 0:
+            return res
+        prev = nums[0]
+        cur = nums[0]
+        for i in range(1, n):
+            if nums[i] - cur != 1:
+                res.append(self.convertToStr(prev, cur))
+                prev = nums[i]
+            cur = nums[i]
+        res.append(self.convertToStr(prev, cur))
+        return res
+
+
+if __name__ == "__main__":
+    #
+    start = time.perf_counter()
+    solution = Solution()
+    print(solution.twoSum(nums = [2 ,7 ,11 ,15], target = 9))
+    end = time.perf_counter()
+    print(f"test 1: {end - start:10.6f} sec")
+    #
+    start2 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,2 ,4], target = 6))
+    end2 = time.perf_counter()
+    print(f"test 2: {end2 - start2:10.6f} sec")
+    #
+    start3 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,3], target = 6))
+    end3 = time.perf_counter()
+    print(f"test 3: {end3 - start3:10.6f} sec")
+    #
+    start4 = time.perf_counter()
+    print(solution.isIsomorphic("badc", "baba"))
+    end4 = time.perf_counter()
+    print(f"test 4: {end4 - start4:10.6f} sec")
+    #
+    start5 = time.perf_counter()
+    print(solution.isIsomorphic("abab", "baba"))
+    end5 = time.perf_counter()
+    print(f"test 5: {end5 - start5:10.6f} sec")
+    #
+    start6 = time.perf_counter()
+    print(solution.firstMissingPositive([1, 1]))
+    end6 = time.perf_counter()
+    print(f"test 6: {end6 - start6:10.6f} sec")
