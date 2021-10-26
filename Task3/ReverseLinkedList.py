3cd37140103541381863df8b9edc64ae9ffb4961
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-10-26 23:30:23 +0300

    add the solution of the LeetCode. Yandex Task List. 206. Reverse Linked List and some drafts

---
+++ Task3/ReverseLinkedList.py
@@ -0,0 +1,123 @@
+
+
+# LeetCode. Yandex Task List. 206. Reverse Linked List
+
+'''
+
+206. Reverse Linked List
+Easy
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+
+Input: head = []
+Output: []
+
+
+
+Constraints:
+
+    The number of nodes in the list is the range [0, 5000].
+    -5000 <= Node.val <= 5000
+
+
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+Accepted
+1,700,169
+Submissions
+2,492,670
+
+
+Related Topics
+Linked List, Recursion
+
+Similar Questions
+Reverse Linked List II
+Medium
+Binary Tree Upside Down
+Medium
+Palindrome Linked List
+Easy
+
+'''
+
+
+import math
+import time
+import collections
+from typing import List, Optional
+import numpy as np
+import random as rnd
+import itertools as it
+from collections import defaultdict, Counter
+import re
+from functools import reduce
+from functools import lru_cache
+from bisect import bisect, bisect_left
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prevPointer = None
+        curPointer = head
+        while curPointer is not None:
+            nextPointer = curPointer.next
+            curPointer.next = prevPointer
+            prevPointer = curPointer
+            curPointer = nextPointer
+        return prevPointer
+
+
+if __name__ == "__main__":
+    #
+    start = time.perf_counter()
+    solution = Solution()
+    print(solution.twoSum(nums = [2 ,7 ,11 ,15], target = 9))
+    end = time.perf_counter()
+    print(f"test 1: {end - start:10.6f} sec")
+    #
+    start2 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,2 ,4], target = 6))
+    end2 = time.perf_counter()
+    print(f"test 2: {end2 - start2:10.6f} sec")
+    #
+    start3 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,3], target = 6))
+    end3 = time.perf_counter()
+    print(f"test 3: {end3 - start3:10.6f} sec")
+    #
+    start4 = time.perf_counter()
+    print(solution.isIsomorphic("badc", "baba"))
+    end4 = time.perf_counter()
+    print(f"test 4: {end4 - start4:10.6f} sec")
+    #
+    start5 = time.perf_counter()
+    print(solution.isIsomorphic("abab", "baba"))
+    end5 = time.perf_counter()
+    print(f"test 5: {end5 - start5:10.6f} sec")
+    #
+    start6 = time.perf_counter()
+    print(solution.firstMissingPositive([1, 1]))
+    end6 = time.perf_counter()
+    print(f"test 6: {end6 - start6:10.6f} sec")
