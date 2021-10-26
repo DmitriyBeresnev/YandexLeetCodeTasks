6b5e5e79f6c284303c852e9a930ed015973c47df
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-10-26 23:22:59 +0300

    add the solution of the LeetCode. Yandex Task List. 443. String Compression

---
+++ Task2/StringСompression.py
@@ -0,0 +1,148 @@
+
+
+# LeetCode. Yandex Task List. 443. String Compression
+
+'''
+
+443. String Compression
+Medium
+
+Given an array of characters chars, compress it using the following algorithm:
+
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+    If the group's length is 1, append the character to s.
+    Otherwise, append the character followed by the group's length.
+
+The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+After you are done modifying the input array, return the new length of the array.
+You must write an algorithm that uses only constant extra space.
+
+
+
+Example 1:
+
+Input: chars = ["a","a","b","b","c","c","c"]
+Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+
+Example 2:
+
+Input: chars = ["a"]
+Output: Return 1, and the first character of the input array should be: ["a"]
+Explanation: The only group is "a", which remains uncompressed since it's a single character.
+
+Example 3:
+
+Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+
+Example 4:
+
+Input: chars = ["a","a","a","b","b","a","a"]
+Output: Return 6, and the first 6 characters of the input array should be: ["a","3","b","2","a","2"].
+Explanation: The groups are "aaa", "bb", and "aa". This compresses to "a3b2a2". Note that each group is independent even if two groups have the same character.
+
+
+
+Constraints:
+
+    1 <= chars.length <= 2000
+    chars[i] is a lowercase English letter, uppercase English letter, digit, or symbol.
+
+Accepted
+207,728
+Submissions
+449,633
+
+
+Related Topics
+Two pointers, String
+
+Similar Questions
+Count and Say
+Medium
+Encode and Decode Strings
+Medium
+Design Compressed String Iterator
+Easy
+Decompress Run-Length Encoded List
+Easy
+
+Hide Hint 1
+How do you know if you are at the end of a consecutive group of characters?
+
+'''
+
+
+import math
+import time
+import collections
+from typing import List
+import numpy as np
+import random as rnd
+import itertools as it
+from collections import defaultdict, Counter
+import re
+from functools import reduce
+from functools import lru_cache
+from bisect import bisect, bisect_left
+
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        res = ""
+        curSymbol = chars[0]
+        curFreq = 0
+        for symbol in chars:
+            if symbol != curSymbol:
+                if curFreq > 1:
+                    res += curSymbol + str(curFreq)
+                else:
+                    res += curSymbol
+                curFreq = 0
+                curSymbol = symbol
+            curFreq += 1
+        if curFreq > 1:
+            res += curSymbol + str(curFreq)
+        else:
+            res += curSymbol
+        chars.clear()
+        chars.extend(list(res))
+        return len(res)
+
+
+if __name__ == "__main__":
+    #
+    start = time.perf_counter()
+    solution = Solution()
+    print(solution.compress(chars = ["a","a","b","b","c","c","c"]))
+    end = time.perf_counter()
+    print(f"test 1: {end - start:10.6f} sec")
+    #
+    start2 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,2 ,4], target = 6))
+    end2 = time.perf_counter()
+    print(f"test 2: {end2 - start2:10.6f} sec")
+    #
+    start3 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,3], target = 6))
+    end3 = time.perf_counter()
+    print(f"test 3: {end3 - start3:10.6f} sec")
+    #
+    start4 = time.perf_counter()
+    print(solution.isIsomorphic("badc", "baba"))
+    end4 = time.perf_counter()
+    print(f"test 4: {end4 - start4:10.6f} sec")
+    #
+    start5 = time.perf_counter()
+    print(solution.isIsomorphic("abab", "baba"))
+    end5 = time.perf_counter()
+    print(f"test 5: {end5 - start5:10.6f} sec")
+    #
+    start6 = time.perf_counter()
+    print(solution.firstMissingPositive([1, 1]))
+    end6 = time.perf_counter()
+    print(f"test 6: {end6 - start6:10.6f} sec")
