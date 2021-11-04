309a8df19a055a123753af2d13e8d09a6e5fde55
Dmitry Beresnev <beresnev.dmitry8@yandex.ru>
2021-11-04 15:38:43 +0300

    add the solution of the LeetCode. Yandex Task List. 1493. Longest Subarray of 1's After Deleting One Element

---
+++ Task9/LongestSubarray.py
@@ -0,0 +1,137 @@
+
+
+# LeetCode. Yandex Task List. 1493. Longest Subarray of 1's After Deleting One Element
+
+'''
+
+1493. Longest Subarray of 1's After Deleting One Element
+Medium
+
+Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+
+
+Example 1:
+
+Input: nums = [1,1,0,1]
+Output: 3
+Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+
+Example 2:
+
+Input: nums = [0,1,1,1,0,1,1,0,1]
+Output: 5
+Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+
+Example 3:
+
+Input: nums = [1,1,1]
+Output: 2
+Explanation: You must delete one element.
+
+Example 4:
+
+Input: nums = [1,1,0,0,1,1,1,0,1]
+Output: 4
+
+Example 5:
+
+Input: nums = [0,0,0]
+Output: 0
+
+
+
+Constraints:
+
+    1 <= nums.length <= 105
+    nums[i] is either 0 or 1.
+
+Accepted
+30,851
+Submissions
+51,601
+
+
+Related Topics
+Math, Dynamic Programming, Sliding Window
+
+Similar Questions
+-
+
+Hide Hint 1
+Maintain a sliding window where there is at most one zero on it.
+
+'''
+
+
+import math
+import time
+import collections
+from typing import List
+import numpy as np
+import random as rnd
+import itertools as it
+from collections import defaultdict, Counter
+import re
+from functools import reduce
+from functools import lru_cache
+from bisect import bisect, bisect_left
+
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        subArrSizesList = list()
+        firstSubArrSize = 0
+        secondSubArrSize = 0
+        n = len(nums)
+        if nums.count(0) == 0:
+            return n-1
+        for i in range(0, n):
+            if nums[i] != 0:
+                firstSubArrSize += 1
+            elif nums[i] == 0:
+                for j in range(i+1, n):
+                    if nums[j] != 0:
+                        secondSubArrSize += 1
+                    else:
+                        break
+                subArrSizesList.append(firstSubArrSize + secondSubArrSize)
+                firstSubArrSize = 0
+                secondSubArrSize = 0
+        return max(subArrSizesList)
+
+
+if __name__ == "__main__":
+    #
+    start = time.perf_counter()
+    solution = Solution()
+    print(solution.twoSum(nums = [2 ,7 ,11 ,15], target = 9))
+    end = time.perf_counter()
+    print(f"test 1: {end - start:10.6f} sec")
+    #
+    start2 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,2 ,4], target = 6))
+    end2 = time.perf_counter()
+    print(f"test 2: {end2 - start2:10.6f} sec")
+    #
+    start3 = time.perf_counter()
+    print(solution.twoSum(nums = [3 ,3], target = 6))
+    end3 = time.perf_counter()
+    print(f"test 3: {end3 - start3:10.6f} sec")
+    #
+    start4 = time.perf_counter()
+    print(solution.isIsomorphic("badc", "baba"))
+    end4 = time.perf_counter()
+    print(f"test 4: {end4 - start4:10.6f} sec")
+    #
+    start5 = time.perf_counter()
+    print(solution.isIsomorphic("abab", "baba"))
+    end5 = time.perf_counter()
+    print(f"test 5: {end5 - start5:10.6f} sec")
+    #
+    start6 = time.perf_counter()
+    print(solution.firstMissingPositive([1, 1]))
+    end6 = time.perf_counter()
+    print(f"test 6: {end6 - start6:10.6f} sec")
